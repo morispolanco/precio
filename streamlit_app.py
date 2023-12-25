be95a78828015f1339ba28fc89b83130e2cb1de6
morispolanco <morispolanco@gmail.com>
2023-12-25 04:05:57 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,47 +1,31 @@
+# Importar las bibliotecas necesarias
 import streamlit as st
-import requests
+import yfinance as yf
 
-# Función para obtener el precio más bajo de un producto en Guatemala
-def get_lowest_price(product_name):
-  # URL de la API de Bard
-  url = "https://api.bard.ai/v1/query"
+# Configuración de la página de Streamlit
+st.title("Consulta de Precio Más Bajo en Guatemala")
+st.sidebar.header("Configuración")
 
-  # Parámetros de la consulta
-  params = {
-    "query": f"precio mas bajo en guatemala de un {product_name}",
-    "language": "es",
-    "model": "PaLM2"
-  }
+# Obtener el símbolo de la acción desde el usuario
+symbol = st.sidebar.text_input("Ingrese el símbolo de la acción (por ejemplo, AAPL):", "AAPL")
 
-  # Enviar la solicitud a la API
-  try:
-    response = requests.get(url, params=params, verify=True)
-  except requests.exceptions.SSLError as e:
-    st.error(f"Error de conexión SSL: {e}")
-    return None
+# Obtener datos históricos de la acción
+st.sidebar.header("Parámetros de la Consulta")
+start_date = st.sidebar.date_input("Fecha de inicio:", pd.to_datetime("2022-01-01"))
+end_date = st.sidebar.date_input("Fecha de fin:", pd.to_datetime("2023-01-01"))
 
-  # Extraer el precio más bajo de la respuesta
-  if response.status_code == 200:
-    data = response.json()
-    lowest_price = data["result"]["price"]
-  else:
-    lowest_price = None
+# Descargar datos históricos
+data = yf.download(symbol, start=start_date, end=end_date)
 
-  return lowest_price
+# Calcular el precio más bajo
+lowest_price = data["Low"].min()
 
-# Interfaz de usuario
-st.title("Precio más bajo en Guatemala")
+# Mostrar el gráfico de precios
+st.subheader(f"Gráfico de precios para {symbol}")
+st.line_chart(data["Close"])
 
-# Campo de texto para ingresar el nombre del producto
-product_name = st.text_input("Nombre del producto")
+# Mostrar el precio más bajo
+st.subheader(f"Precio más bajo para {symbol}: {lowest_price}")
 
-# Botón para obtener el precio más bajo
-if st.button("Obtener precio"):
-  # Obtener el precio más bajo
-  lowest_price = get_lowest_price(product_name)
-
-  # Mostrar el precio más bajo
-  if lowest_price is not None:
-    st.success(f"El precio más bajo es de Q{lowest_price}")
-  else:
-    st.error("No se pudo encontrar el precio más bajo")
+# Información adicional
+st.sidebar.markdown("Datos obtenidos de Yahoo Finance.")
