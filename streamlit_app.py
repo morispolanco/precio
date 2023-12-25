2acf3def649fb11155536c55cfab19f87fbfa51f
morispolanco <morispolanco@gmail.com>
2023-12-25 04:53:20 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,58 +1,23 @@
 import streamlit as st
 import requests
-from urllib3.util.ssl_ import create_urllib3_context
 
-# Función para obtener el precio más bajo de un producto en Guatemala
-def get_lowest_price(product_name):
-    # Deshabilitar advertencias de InsecureRequestWarning
-    requests.packages.urllib3.disable_warnings(requests.packages.urllib3.exceptions.InsecureRequestWarning)
+def get_best_price(product_name: str, location: str) -> str:
+    url = f"https://www.google.com/search?q={product_name}+price+in+{location}"
+    headers = {
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3"}
+    res = requests.get(url, headers=headers)
+    data = res.text
+    start = data.find("1.")
+    end = data.find("2.")
+    return data[start:end]
 
-    # Configurar la versión del protocolo SSL/TLS
-    context = create_urllib3_context()
-    context.set_ciphers('DEFAULT@SECLEVEL=1')  # Ajuste para SSL/TLS modernos
-    context.options |= getattr(context, 'OP_NO_TLSv1_3', 0)
-    context.options |= getattr(context, 'OP_NO_TLSv1_2', 0)
-    context.options |= getattr(context, 'OP_NO_TLSv1_1', 0)
+def app():
+    st.title("Encuentra el mejor precio de un producto en Guatemala")
+    product_name = st.text_input("Ingresa el nombre del producto")
+    location = st.text_input("Ingresa la ubicación")
+    if st.button("Buscar"):
+        result = get_best_price(product_name, location)
+        st.write(result)
 
-    # URL de la API de Bard
-    url = "https://api.bard.ai/v1/query"
-
-    # Parámetros de la consulta
-    params = {
-        "query": f"precio mas bajo en guatemala de un {product_name}",
-        "language": "es",
-        "model": "PaLM2"
-    }
-
-    # Enviar la solicitud a la API
-    try:
-        response = requests.get(url, params=params, verify=False, timeout=10, context=context)
-    except requests.exceptions.SSLError as e:
-        st.error(f"Error de conexión SSL: {e}")
-        return None
-
-    # Extraer el precio más bajo de la respuesta
-    if response.status_code == 200:
-        data = response.json()
-        lowest_price = data["result"]["price"]
-    else:
-        lowest_price = None
-
-    return lowest_price
-
-# Interfaz de usuario
-st.title("Precio más bajo en Guatemala")
-
-# Campo de texto para ingresar el nombre del producto
-product_name = st.text_input("Nombre del producto")
-
-# Botón para obtener el precio más bajo
-if st.button("Obtener precio"):
-    # Obtener el precio más bajo
-    lowest_price = get_lowest_price(product_name)
-
-    # Mostrar el precio más bajo
-    if lowest_price is not None:
-        st.success(f"El precio más bajo es de Q{lowest_price}")
-    else:
-        st.error("No se pudo encontrar el precio más bajo")
+if __name__ == "__main__":
+    app()
