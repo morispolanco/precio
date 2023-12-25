3153bca956489db6ff2dc9f2dae493ea02a13189
morispolanco <morispolanco@gmail.com>
2023-12-25 05:09:55 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import os
 import openai
 
-openai.api_key = st.secrets["OPENAI_API_KEY"]
+openai.api_key = os.getenv("OPENAI_API_KEY")
 
 def generate_text(prompt):
     completions = openai.Completion.create(
